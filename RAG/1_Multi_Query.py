bc5b965ecaedc1bb0763d416e5bbc30daf76f446
cidkdXiao <cidkdxiao@foxmail.com>
2025-11-03 13:04:27 +0800

    first

--- RAG/1_Multi_Query.py
+++ RAG/1_Multi_Query.py
@@ -6,6 +6,8 @@ from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from langchain.schema.runnable import RunnableLambda
 from langchain.load import dumps, loads
+from operator import itemgetter
+from langchain_core.runnables import RunnablePassthrough
 import bs4
 
 # 1) Load
@@ -62,3 +64,54 @@ generate_queries = (
     | (lambda x: ensure_five(x.splitlines()))
     | post_clean
 )
+
+
+# 6) Retrieval QA with Multi-Query
+# def get_unique_union(documents: list[list]):
+#     """Unique union of retrieved docs"""
+#     # Flatten list of lists, and convert each Document to string
+#     flattened_docs = [dumps(doc) for sublist in documents for doc in sublist]
+#     # Get unique documents
+#     unique_docs = list(set(flattened_docs))
+#     # Return
+#     return [loads(doc) for doc in unique_docs]
+
+def unique_union_ordered(doc_lists: list[list]):
+    seen = set()
+    merged = []
+    for sub in doc_lists:                 # 保留先出现的优先级
+        for d in sub:
+            key = (d.page_content,)       # 仅按内容去重，或加 source 等
+            if key not in seen:
+                seen.add(key)
+                merged.append(d)
+    return merged
+
+# Retrieve
+question = "What is task decomposition for LLM agents?"
+retrieval_chain = generate_queries | retriever.map() | unique_union_ordered
+docs = retrieval_chain.invoke({"question":question})
+
+
+# 7) Responding with Retrieved Context
+# RAG
+QA_template = """Answer the following question based on this context:
+
+{context}
+
+Question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(QA_template)
+
+llm = ChatOpenAI(temperature=0)
+
+final_rag_chain = (
+    {"context": retrieval_chain,
+     "question": itemgetter("question")}
+    | prompt
+    | llm
+    | StrOutputParser()
+)
+
+final_rag_chain.invoke({"question": question})
